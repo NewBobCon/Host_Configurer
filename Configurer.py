544131434af7efce6464aab4c252736b451fb6a6
Conor McIntosh <mcintc2@uw.edu>
2021-11-18 15:39:16 -0800

    Added variable username and password, along with timeout and password authorization.

--- Configurer.py
+++ Configurer.py
@@ -41,11 +41,16 @@ def getCommands():
 hosts = getHosts() #obtain the hosts
 commands = getCommands() #obtain the commands
 #for every host in the hosts list, ssh into that host and run all the commands
+print("Type username: ")
+username = sys.stdin.readline().strip()
+print("Type password: ")
+password = sys.stdin.readline().strip()
 for host in hosts:
     #os.system("ssh chume@%s " %host) #SSH into the host
     connection = paramiko.SSHClient()
     connection.set_missing_host_key_policy(AutoAddPolicy)
-    connection.connect(host, username='eric', password='c')
+    connection.connect(host, username=username, password=password, timeout=10)
+    connection.authorize(password=password)
     for command in commands: #For ever command in the commands variable
         #output = os.popen(command).read() #Run the command and catpture the output from the console
         stdin, stdout, stderr = connection.exec_command(command)
