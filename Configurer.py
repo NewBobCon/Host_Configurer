4d0893262cbd66c3496f8a699f5c54ef2121132b
Conor McIntosh <mcintc2@uw.edu>
2022-01-08 11:52:37 -0800

    Added more functionality to command execution loop

--- Configurer.py
+++ Configurer.py
@@ -46,16 +46,16 @@ username = sys.stdin.readline().strip()
 print("Input password: ")
 password = sys.stdin.readline().strip()
 for host in hosts:
-    #os.system("ssh chume@%s " %host) #SSH into the host
     connection = paramiko.SSHClient()
     connection.set_missing_host_key_policy(AutoAddPolicy)
-    connection.connect(host, username=username, password=password, timeout=10)
+    connection.connect(host, username=username, password=password, timeout=10) #SSH into the host
     connection.load_system_host_keys()
     for command in commands: #For every command in the commands variable
-        #output = os.popen(command).read() #Run the command and catpture the output from the console
-        stdin, stdout, stderr = connection.exec_command(command)
-        if "'%' invalid input error" in stdout or "SHELL PARSER FAILURE:" in stdout: #Check for errors from the output
+        stdin, stdout, stderr = connection.exec_command(command, len(command), 5) #Run the command and catpture the output from the console
+        if "'%' invalid input error" in stderr or "SHELL PARSER FAILURE:" in stderr: #Check for errors from the output
             print("Error: command failed to run")
+            print("Output from console: %s", stdout)
+            print("Error from console: %s", stderr)
             quit()
         print(stdout)
 print("Successfully completed all commands for every host")
